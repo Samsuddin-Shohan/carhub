4a9aef5390332206532b2ae669c5b7f9f77bf19e
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-04-09 23:49:04 +0600

    server side connected

---
+++ src/pages/AllProducts/AllProducts.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
+import AllProduct from "../AllProduct/AllProduct";
+import Footer from "../Home/Shared/Footer/Footer";
+import Navigation from "../Home/Shared/Navigation/Navigation";
+import "../Products/Products.css";
+
+<Spinner animation="border" role="status">
+  <span className="visually-hidden">Loading...</span>
+</Spinner>;
+
+const AllProducts = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <>
+      <Navigation />
+      <div>
+        <h2 className="text-primary mt-5">Our Products</h2>
+      </div>
+      <div className="products-container" id="products">
+        {products.map((item) => (
+          <AllProduct key={item._id} item={item}></AllProduct>
+        ))}
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default AllProducts;
---
+++ src/pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1-cpPZ5Hs-mfA-g_aiC1fzJbdzwk1Hek",
+  authDomain: "motor-home-fc8fb.firebaseapp.com",
+  projectId: "motor-home-fc8fb",
+  storageBucket: "motor-home-fc8fb.appspot.com",
+  messagingSenderId: "522233255507",
+  appId: "1:522233255507:web:f9622ae0dd5c6ea0c6d8a6"
+};
+
+export default firebaseConfig;
